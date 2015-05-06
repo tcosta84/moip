2017b2c279138cdd44199614290e4be6e8c5fc55
Thiago Costa <thiago.costa@acotel.com>
2015-05-06 19:13:06 -0300

    Testa uma nova forma com mixins

--- moip/__init__.py
+++ moip/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import json
 import base64
 import requests
 
@@ -12,7 +13,8 @@ def remove_last_trailling_slash(string):
 
 
 class ApiError(Exception):
-    def __init__(self, message=None, errors=None):
+    def __init__(self, status_code=None, message=None, errors=None):
+        self.status_code = status_code
         self.message = message
         self.errors = errors
 
@@ -34,29 +36,205 @@ class MoipAssinaturas(object):
         }
 
     def get_resource(self, resource):
+        if resource == 'plan':
+            return Plan(self.api_url, self.headers)
         if resource == 'subscription':
             return Subscription(self.api_url, self.headers)
 
 
 class BaseResource(object):
-    def handle_return(self, response):
-        content = response.json()
+    def _handle_return(self, response, return_content=True):
         if response.status_code == 200 or response.status_code == 201:
-            return content
+            if return_content:
+                return response.json()
         else:
-            if response.status_code == 400:
-                raise ApiError(message=content['message'], errors=content['errors'])
+            if response.content:
+                content = response.json()
+                raise ApiError(status_code=response.status_code, message=content['message'], 
+                        errors=content['errors'])
             else:
-                raise ApiError()
+                raise ApiError(status_code=response.status_code)
 
 
-class Subscription(BaseResource):
+class CreateMixin(object):
+    def create(self, data):
+        url = '{0}'.format(self.url)
+        response = requests.post(url, data=json.dumps(data), headers=self.headers)
+        return self._handle_return(response)
+
+
+class DetailMixin(object):
+    def get(self, code):
+        url = '{0}/{1}'.format(self.url, code)
+        response = requests.get(url, headers=self.headers)
+        return self._handle_return(response)
+
+
+class ListMixin(object):
+    def all(self, limit=100, offset=0):
+        url = '{0}?limit={1}&offset={2}'.format(self.url, limit, offset)
+        response = requests.get(url, headers=self.headers)
+        return self._handle_return(response)
+
+
+class UpdateMixin(object):
+    def update(self, code, data):
+        url = '{0}/{1}'.format(self.url, code)
+        response = requests.put(url, data=json.dumps(data), headers=self.headers)
+        return self._handle_return(response, return_content=False)
+
+
+class ActivateMixin(object):
+    def activate(self, code):
+        url = '{0}/{1}/activate'.format(self.url, code)
+        data = None
+        response = requests.put(url, data=data, headers=self.headers)
+        return self._handle_return(response, return_content=False)
+
+
+class InactivateMixin(object):
+    def inactivate(self, code):
+        url = '{0}/{1}/inactivate'.format(self.url, code)
+        data = None
+        response = requests.put(url, data=data, headers=self.headers)
+        return self._handle_return(response, return_content=False)
+
+
+class SuspendMixin(object):
+    def suspend(self, code):
+        url = '{0}/{1}/suspend'.format(self.url, code)
+        data = None
+        response = requests.put(url, data=data, headers=self.headers)
+        return self._handle_return(response, return_content=False)
+
+
+class CancelMixin(object):
+    def cancel(self, code):
+        url = '{0}/{1}/cancel'.format(self.url, code)
+        data = None
+        response = requests.put(url, data=data, headers=self.headers)
+        return self._handle_return(response, return_content=False)
+
+
+class RetryMixin(object):
+    def retry(self, code):
+        url = '{0}/{1}/retry'.format(self.url, code)
+        data = None
+        response = requests.put(url, data=data, headers=self.headers)
+        return self._handle_return(response, return_content=False)
+
+
+class Plan(BaseResource, CreateMixin, ListMixin, DetailMixin, UpdateMixin, ActivateMixin,
+        InactivateMixin):
+    def __init__(self, api_url, headers):
+        self.api_url = api_url
+        self.url = '{0}/plans'.format(self.api_url)
+        self.headers = headers
+
+
+class Customer(BaseResource, CreateMixin, ListMixin, DetailMixin, UpdateMixin):
+    def __init__(self, api_url, headers):
+        self.api_url = api_url
+        self.url = '{0}/customers'.format(self.api_url)
+        self.headers = headers
+
+
+class Subscription(BaseResource, CreateMixin, ListMixin, DetailMixin, UpdateMixin, ActivateMixin,
+        SuspendMixin, CancelMixin):
     def __init__(self, api_url, headers):
         self.api_url = api_url
         self.url = '{0}/subscriptions'.format(self.api_url)
         self.headers = headers
 
-    def all(self):
-        url = self.url
-        response = requests.get(url, headers=self.headers)
-        return self.handle_return(response)
+
+class Invoice(BaseResource, ListMixin, DetailMixin, RetryMixin):
+    def __init__(self, api_url, headers):
+        self.api_url = api_url
+        self.url = '{0}/invoices'.format(self.api_url)
+        self.headers = headers
+
+
+class Payment(BaseResource, ListMixin, DetailMixin):
+    def __init__(self, api_url, headers):
+        self.api_url = api_url
+        self.url = '{0}/payments'.format(self.api_url)
+        self.headers = headers
+
+
+class Preference(BaseResource, ListMixin, DetailMixin):
+    def __init__(self, api_url, headers):
+        self.api_url = api_url
+        self.url = '{0}/payments'.format(self.api_url)
+        self.headers = headers
+
+
+# class Subscription(BaseResource):
+#     def __init__(self, api_url, headers):
+#         self.api_url = api_url
+#         self.url = '{0}/subscriptions'.format(self.api_url)
+#         self.headers = headers
+#
+#     def create(self, code, amount, plan_code, customer_code):
+#         url = '{0}'.format(self.url)
+#         data = {
+#             'code': code,
+#             'amount': amount,
+#             'plan': {
+#                 'code': plan_code
+#             },
+#             'customer': {
+#                 'code': customer_code
+#             }
+#         }
+#         response = requests.post(url, data=json.dumps(data), headers=self.headers)
+#         return self._handle_return(response)
+#
+#     def all(self, limit=100, offset=0):
+#         url = '{0}?limit={1}&offset={2}'.format(self.url, limit, offset)
+#         response = requests.get(url, headers=self.headers)
+#         return self._handle_return(response)
+#
+#     def get(self, code):
+#         url = '{0}/{1}'.format(self.url, code)
+#         response = requests.get(url, headers=self.headers)
+#         return self._handle_return(response)
+#
+#     def update(self, code, plan_code=None, amount=None, next_invoice_date=None):
+#         url = '{0}/{1}'.format(self.url, code)
+#         data = {}
+#         if plan_code:
+#             data.update({
+#                 'plan': {
+#                     'code': plan_code
+#                 }
+#             })
+#         if amount:
+#             data.update({'amount': amount})
+#         if next_invoice_date:
+#             data.update({
+#                 'next_invoice_date': {
+#                     'day': next_invoice_date.day,
+#                     'month': next_invoice_date.month,
+#                     'year': next_invoice_date.year
+#                 }
+#             })
+#         response = requests.put(url, data=json.dumps(data), headers=self.headers)
+#         return self._handle_return(response, return_content=False)
+#
+#     def suspend(self, code):
+#         url = '{0}/{1}/suspend'.format(self.url, code)
+#         data = None
+#         response = requests.put(url, data=data, headers=self.headers)
+#         return self._handle_return(response, return_content=False)
+#
+#     def activate(self, code):
+#         url = '{0}/{1}/activate'.format(self.url, code)
+#         data = None
+#         response = requests.put(url, data=data, headers=self.headers)
+#         return self._handle_return(response, return_content=False)
+#
+#     def cancel(self, code):
+#         url = '{0}/{1}/cancel'.format(self.url, code)
+#         data = None
+#         response = requests.put(url, data=data, headers=self.headers)
+#         return self._handle_return(response, return_content=False)
--- moip/tests/test_apis.py
+++ moip/tests/test_apis.py
@@ -2,25 +2,313 @@
 
 import pytest
 
+from httmock import all_requests, response,  HTTMock
+
 from moip import MoipAssinaturas, ApiError
 
 
-def test_subscriptions():
+@all_requests
+def response_content_ok(url, request):
+    HEADERS = {'content-type': 'application/json'}
+    content = {}
+    return response(200, content, HEADERS, None, 5, request)
+
+
+@all_requests
+def response_content_bad_request(url, request):
+    HEADERS = {'content-type': 'application/json'}
+    content = {
+        'message': 'Erro na requisicao',
+        'errors': [
+            {
+                'code': 'MA1',
+                'description': 'Codigo ja utilizado. Escolha outro codigo.'
+            }
+        ]
+    }
+    return response(400, content, HEADERS, None, 5, request)
+
+
+@all_requests
+def response_content_error(url, request):
+    HEADERS = {'content-type': 'application/json'}
+    content = None
+    return response(405, content, HEADERS, None, 5, request)
+
+
+def test_create_plan():
     moip = MoipAssinaturas(
         api_url='https://sandbox.moip.com.br/assinaturas/v1/',
         api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
         api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
     )
 
+    data = {
+        "code": "plano100",
+        "name": "Plano Especial",
+        "description": "Descrição do Plano Especial",
+        "amount": 990,
+        "setup_fee": 500,
+        "max_qty": 1,
+        "status": "ACTIVE",
+        "interval": {
+            "length": 1,
+            "unit": "MONTH"
+        },
+        "billing_cycles": 12,
+        "trial": {
+            "days": 30,
+            "enabled": True,
+            "hold_setup_fee": True
+        }
+    }
+
+    plan = moip.get_resource('plan')
+    with HTTMock(response_content_ok):
+        response = plan.create(data=data)
+
+    assert isinstance(response, dict) == True
+
+
+def test_get_plan():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    plan = moip.get_resource('plan')
+    with HTTMock(response_content_ok):
+        response = plan.get(code='plano100')
+
+    assert isinstance(response, dict) == True
+
+
+def test_get_plans():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    plan = moip.get_resource('plan')
+    with HTTMock(response_content_ok):
+        response = plan.all()
+
+    assert isinstance(response, dict) == True
+
+
+def test_update_plan():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    data = {
+        "code": "plano101",
+        "name": "Novo Plano Especial 100",
+        "description": "Descrição do Plano Especial",
+        "amount": 990,
+        "setup_fee": 500,
+        "max_qty": 1,
+        "status": "ACTIVE",
+        "interval": {
+            "length": 1,
+            "unit": "MONTH"
+        },
+        "billing_cycles": 12,
+        "trial": {
+            "days": 30,
+            "enabled": True,
+            "hold_setup_fee": True
+        }
+    }
+
+    plan = moip.get_resource('plan')
+    with HTTMock(response_content_ok):
+        response = plan.update(code='plano100', data=data)
+
+    assert response is None
+
+
+def test_activate_plan():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    plan = moip.get_resource('plan')
+    with HTTMock(response_content_ok):
+        response = plan.activate(code='plano100')
+
+    assert response is None
+
+
+def test_inactivate_plan():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    plan = moip.get_resource('plan')
+    with HTTMock(response_content_ok):
+        response = plan.inactivate(code='plano100')
+
+    assert response is None
+
+
+def test_create_subscription():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+    data = {
+        'code': 'assinatura100',
+        'amount': '9990',
+        'plan': {
+            'code': 'plano01'
+        },
+        'customer': {
+            'code': 'cliente01'
+        }
+    }
     subscription = moip.get_resource('subscription')
-    assert isinstance(subscription.all(), dict) == True
+    with HTTMock(response_content_ok):
+        response = subscription.create(data=data)
+
+    assert isinstance(response, dict) == True
 
 
-def test_subscriptions_exception(mocker):
-    requests_get = mocker.patch('moip.requests.get')
-    requests_get.return_value.status_code = 400
-    requests_get.return_value.json.return_value = {'message': 'Erro', 'errors': []}
+def test_list_subscriptions():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.all()
 
+    assert isinstance(response, dict) == True
+
+
+def test_get_subscription():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.get(code='assinatura100')
+
+    assert isinstance(response, dict) == True
+
+
+def test_update_subscription_plan():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+    data = {
+        'plan': {
+            'code': 'plano01'
+        }
+    }
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.update(code='assinatura100', data=data)
+
+    assert response is None
+
+
+def test_update_subscription_amount():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    data = {
+        'amount': '9990'
+    }
+
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.update(code='assinatura100', data=data)
+
+    assert response is None
+
+
+def test_update_subscription_next_invoice_date():
+    import datetime
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    next_invoice_date = datetime.date.today() + datetime.timedelta(days=1)
+    data = {
+        'next_invoice_date': {
+            'day': next_invoice_date.day,
+            'month': next_invoice_date.month,
+            'year': next_invoice_date.year
+        }
+    }
+
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.update(code='assinatura100', data=data) 
+
+    assert response is None
+
+
+def test_suspend_subscription():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.suspend(code='assinatura100')
+
+    assert response is None
+
+
+def test_activate_subscription():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.activate(code='assinatura100')
+
+    assert response is None
+
+
+def test_cancel_subscription():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+    subscription = moip.get_resource('subscription')
+    with HTTMock(response_content_ok):
+        response = subscription.cancel(code='assinatura100')
+
+    assert response is None
+
+
+def test_subscriptions_bad_request_exception():
     moip = MoipAssinaturas(
         api_url='https://sandbox.moip.com.br/assinaturas/v1/',
         api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
@@ -29,7 +317,24 @@ def test_subscriptions_exception(mocker):
 
     subscription = moip.get_resource('subscription')
     with pytest.raises(ApiError) as e:
-        subscription.all()
+        with HTTMock(response_content_bad_request):
+            subscription.all()
 
-    assert e.value.message == 'Erro'
+    assert e.value.message == 'Erro na requisicao'
     assert isinstance(e.value.errors, list) == True
+
+
+def test_subscriptions_any_status_code_exception():
+    moip = MoipAssinaturas(
+        api_url='https://sandbox.moip.com.br/assinaturas/v1/',
+        api_token='D3B1BQVJ15UF3ZXOYGQMD30JGJQTX0BM',
+        api_key='Z30OJNXCSYZ2GQNX2YKPC7VSSPMC7TMFL4ZLEKFH'
+    )
+
+    subscription = moip.get_resource('subscription')
+    with pytest.raises(ApiError) as e:
+        with HTTMock(response_content_error):
+            subscription.all()
+
+    assert e.value.message is None
+    assert e.value.errors is None
